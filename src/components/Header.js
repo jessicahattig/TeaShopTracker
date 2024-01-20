7d4ec0c4275239e938575aa61fc8284f380da1c0
jessicahattig <jessicahattig@gmail.com>
2024-01-20 15:03:14 -0800

    Add styling to header.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,29 @@
 import React from "react";
-import teaImage from "./../img/tea.png"
+import teaImage from "./../img/tea.png";
 
 function Header() {
-  return (
-    <>
-      <img src={teaImage} alt="Tea" />
-      <h1>Welcome to the Tea Shop Tracker</h1>
-    </>
+  const headerStyle = {
+    textAlign: 'center',
+    padding: '20px',
+    backgroundColor: '#f7f7f7',
+  };
 
+  const imageStyle = {
+    width: '300px',
+    height: 'auto',
+  };
+
+  const titleStyle = {
+    fontFamily: 'Arial, sans-serif',
+    color: '#333',
+    marginTop: '10px',
+  };
+
+  return (
+    <div style={headerStyle}>
+      <img src={teaImage} alt="Tea" style={imageStyle} />
+      <h1 style={titleStyle}>Welcome to the Tea Shop Tracker</h1>
+    </div>
   );
 }
 
