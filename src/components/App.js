5cc483dbf3329c92818cc76621c9b62b39dec8af
jessicahattig <jessicahattig@gmail.com>
2024-01-20 11:19:27 -0800

    Create new project with react framework.

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header";
+import TeaControler from "./TeaControler";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <TeaControl />
+    </>
+  );
+}
+
+export default App;
